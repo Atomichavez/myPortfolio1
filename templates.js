1511ca9ac15103999c07e0c7b7bab5ec9e7588c5
atomichavez <dchavez0628@gmail.com>
2022-02-20 14:06:49 -0600

    No funciona pero hare cambios mayores

--- templates.js
+++ templates.js
@@ -2,19 +2,19 @@
 const nav = document.createElement('nav');
 nav.innerHTML = 
 `   <a id="logo" href="/index.html">
-        <span>DAVID CHAVEZ</span>
+    <span>DAVID CHAVEZ</span>
     </a>
     <ul>
         <li><a href="/about.html>ABOUT</a></li>
         <li><a href="/projects.html>PROJECTS</a></li>
         <li><a href="/contact.html>CONTACT</a></li>
     </ul>`;
-var getHeader = document.getElementsByTagName('header');
-getHeader.appendChild(nav.content);
+var getHeader = document.getElementsByTagName('header')[0];
+getHeader.appendChild(nav);
 
 //Footer for all the pages
 const footnote = document.createElement('div');
 footnote.innerHTML = 
     `<p>Last updated 2022</p>`;
-var getFooter = document.getElementsByTagName('footer');
+var getFooter = document.getElementsByTagName('footer')[0];
 getFooter.appendChild(footnote.content);
