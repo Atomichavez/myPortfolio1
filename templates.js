4dcb3edd60ead879e585265ff5c722ec4cf97561
atomichavez <dchavez0628@gmail.com>
2022-02-20 14:23:02 -0600

    major changes made to the javascript template

--- templates.js
+++ templates.js
@@ -1,20 +1,27 @@
 //Header for all the pages
-const nav = document.createElement('nav');
-nav.innerHTML = 
-`   <a id="logo" href="/index.html">
+const htmlStr = 
+`
+<nav>   
+    <a id="logo" href="/index.html">
     <span>DAVID CHAVEZ</span>
     </a>
     <ul>
         <li><a href="/about.html>ABOUT</a></li>
         <li><a href="/projects.html>PROJECTS</a></li>
         <li><a href="/contact.html>CONTACT</a></li>
-    </ul>`;
-var getHeader = document.getElementsByTagName('header')[0];
-getHeader.appendChild(nav);
+    </ul>
+</nav>
+`
+const davidNavs = document.getElementsByTagName('david-nav');
+Object.values(davidNavs).forEach(davidNav => {
+    davidNav.innerHTML = htmlStr
+})
 
-//Footer for all the pages
+/*
+Footer for all the pages
 const footnote = document.createElement('div');
 footnote.innerHTML = 
     `<p>Last updated 2022</p>`;
 var getFooter = document.getElementsByTagName('footer')[0];
 getFooter.appendChild(footnote.content);
+*/
